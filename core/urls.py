63cf9571c0ee91b86caa38910c5db1698638d0b4
GordenArcher <archergorden@gmail.com>
2024-11-23 01:44:24 +0000

    updated urls

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/", include("memory.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('secret-admin/', admin.site.urls),  
+    path("api/v1/", include("memory.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
